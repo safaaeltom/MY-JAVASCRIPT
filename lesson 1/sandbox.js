0bae8ed5d86079febe50a10cbfbe236a1e982b27
Safaa <safaaeltom@gmail.com>
2026-05-10 22:27:38 +0300

    Added string basics to repo

---
+++ lesson 1/sandbox.js
@@ -0,0 +1,37 @@
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+age = 30;
+console.log(age);
+
+const points = 100
+console.log(points);
+
+/* comment out
+multiple lines */
+// STRINGS - comment out single line
+
+// string concatenation
+let firstName = 'Brad';
+let lastName = 'Bitt';
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+// getting character
+console.log(fullName[3]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+// index in strings
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+let index = email.indexOf('@');
+console.log(index)
