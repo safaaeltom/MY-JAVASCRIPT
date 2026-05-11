9339662bfc3e56a6f83973b73077b9cc07b9039c
Safaa <safaaeltom@gmail.com>
2026-05-11 17:43:24 +0300

    Practice boolean logic and comparison operators

---
+++ Boolean/sandbox.js
@@ -0,0 +1,38 @@
+console.log(true, false, 'true', 'false');
+
+//let email = 'luigi@thenetninja.co.uk';
+//let result = email.includes('@'); <<checks if it includes @
+//console.log(result);
+
+// camparison operators for numbers/strings
+// let age = 25;
+// console.log(age==25);
+// console.log(age!=25);
+// console.log(age<25);
+// console.log(age>=25);
+
+//let age = 25;
+//console.log(age == '25');
+//console.log(age != '25');
+
+//strict comparison
+//console.log(age===25);
+//console.log(age==='25');
+
+let score = '100';
+//score = Number(score);
+//console.log(score + 1);
+
+//console.log(typeof score);
+
+//let result = Number('hello');
+//console.log(result);
+
+// let result = String(50);
+// console.log(result, typeof result);
+
+// +ve/-ve numbers (truthy values), 
+// 0 is falsy value
+
+// let result = Boolean(100); 
+// console.log(result, typeof result);
