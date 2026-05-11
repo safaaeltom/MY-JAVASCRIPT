a2dc30a10711852f729d59529ed5e3388c14b70f
Safaa <safaaeltom@gmail.com>
2026-05-11 15:13:21 +0300

    Practice JavaScript array properties and methods

---
+++ Arrays/sandbox.js
@@ -0,0 +1,27 @@
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+//ninjas[1] = 'ken'; <<replaces the first name
+//console.log(ninjas[1]); 
+
+//let random = ['shaun',25,'ryu',35];
+//console.log(random);
+
+// arrays properties
+//console.log(ninjas.length)
+
+// arrays methods
+//let result = ninjas.join('.'); <<joins the components with '.'
+//console.log (result);
+
+//let result = ninjas.indexOf('ryu');
+//console.log(result);
+
+//let result = ninjas.concat(['ken', 'crystal']); <<adds another array
+//console.log(result);
+
+//let result = ninjas.push('safaa'); <<adds this value to the array
+//console.log(result); <<gives number of values
+//console.log(ninjas); <<gives the new array
+
+//let result = ninjas.pop(); <<takes away the last component
+//console.log(result); <<gives the poped value
+//console.log(ninjas); <<gives the new array
