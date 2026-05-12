3c156915b35e0f119d1d49ad80bc5625586ee529
Safaa <safaaeltom@gmail.com>
2026-05-12 13:18:41 +0300

    Complete javascript do while loops revision

--- Conditionals/sandbox.js
+++ Conditionals/sandbox.js
@@ -3,17 +3,16 @@
 // for(let i=0; i<5; i++){ 
 //     console.log('in loop:', i);
 // }
-// console.log('loop finished');
 
 // const names = ['shaun', 'mario', 'luigi'];
-
 //  for (let i=0; i<names.length; i++){
-// //     console.log(names[i]);
+//     console.log(names[i]);
+
 // let html = `<div>${names[i]}</div>`;
 // console.log(html);
 // }
 
-// WHILE LOOPS //
+// WHILE LOOPS // <evaluates the condition before running the code
 
 // let i=0
 // while(i<5){ 
@@ -21,9 +20,9 @@
 //      i++;
 // }
 
-const names = ['shaun', 'mario', 'luigi'];
-let i=0
-while(i<names.length){ 
-     console.log(names[i]);
-     i++;
-}
+// DO WHILE LOOPS <<runs the code regardless the condition then evaluates the condition
+// let i=5
+//   do{ 
+//     console.log('value of i is:', i);
+//     i++;
+//   } while(i<5);
