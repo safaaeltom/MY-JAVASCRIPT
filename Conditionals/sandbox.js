2937b869652a23cd0f7eba2809e2489335107eee
Safaa <safaaeltom@gmail.com>
2026-05-12 13:03:47 +0300

    Finish javascript while loops revision

--- Conditionals/sandbox.js
+++ Conditionals/sandbox.js
@@ -1,13 +1,29 @@
-// for loops
+// FOR LOOPS //
+
 // for(let i=0; i<5; i++){ 
 //     console.log('in loop:', i);
 // }
 // console.log('loop finished');
 
-const names = ['shaun', 'mario', 'luigi'];
+// const names = ['shaun', 'mario', 'luigi'];
 
- for (let i=0; i<names.length; i++){
-//     console.log(names[i]);
-let html = `<div>${names[i]}</div>`;
-console.log(html);
+//  for (let i=0; i<names.length; i++){
+// //     console.log(names[i]);
+// let html = `<div>${names[i]}</div>`;
+// console.log(html);
+// }
+
+// WHILE LOOPS //
+
+// let i=0
+// while(i<5){ 
+//      console.log('in loop:', i);
+//      i++;
+// }
+
+const names = ['shaun', 'mario', 'luigi'];
+let i=0
+while(i<names.length){ 
+     console.log(names[i]);
+     i++;
 }
