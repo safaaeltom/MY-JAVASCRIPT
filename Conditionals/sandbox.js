e5627e8c49a608aebb8fee84b8c29cdcf28577ff
Safaa <safaaeltom@gmail.com>
2026-05-14 13:51:21 +0300

    Complete javascript loops continue revision

--- Conditionals/sandbox.js
+++ Conditionals/sandbox.js
@@ -81,6 +81,10 @@
 
 const scores = [50, 25, 0, 30, 100, 20, 10];
 for (let i=0; i<scores.length; i++){
+  if(scores[i]===0){
+    continue;
+  }
+
   console.log('Your score: ', scores[i]);
 
   if(scores[i]===100){
