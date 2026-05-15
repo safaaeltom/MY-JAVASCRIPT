77aec2aad07695c72998bfc576c62a0c3e788800
Safaa <safaaeltom@gmail.com>
2026-05-15 22:12:50 +0300

    Finish arguments & parameters

--- Functions/sandbox.js
+++ Functions/sandbox.js
@@ -19,13 +19,40 @@
 // speak();
 
 // JS HOISTING WORKS ONLY WITH FUNCTION DECLARATION
-const speak = function(){
-    console.log('Good day!');
-};
+// const speak = function(){
+//     console.log('Good day!');
+// };
 
-greet();
-greet();
+// greet();
+// greet();
 
-function greet(){
-    console.log('Hello there');
-}
+// function greet(){
+//     console.log('Hello there');
+// }
+
+// ARGUMENTS & PARAMETERS
+// const speak = function(){
+//     console.log('good day!');
+// };
+
+// speak();
+
+// const speak = function(name){
+//   console.log(`good day ${name}!`);
+// };
+
+// speak('Mario');
+
+// const speak = function(name, time){
+//    console.log(`good ${time} ${name}!`);
+// };
+
+// speak('Mario', 'morning');
+
+// const speak = function(name='luigi', time='night'){
+//    console.log(`good ${time} ${name}!`);
+// };
+
+// speak();
+// speak('shaun');
+// speak('shaun', 'morning');
