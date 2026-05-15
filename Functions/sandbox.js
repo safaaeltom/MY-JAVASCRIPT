ab70717dfef3233f9c4c0433ee42296dd58d71ba
Safaa <safaaeltom@gmail.com>
2026-05-15 23:39:21 +0300

    Complete regular and arrow function exercise

--- Functions/sandbox.js
+++ Functions/sandbox.js
@@ -57,10 +57,42 @@
 // speak('shaun');
 // speak('shaun', 'morning');
 
-// RETURNING VALUES
+// RETURNING VALUES (to reuse a value)
 // const calcArea = function(radius){
 //     return 3.14*radius**2;
 // };
 // const area = calcArea(10);
 // console.log('Answer is: ', area);
 
+// REGULAR FUNCTION
+// const calcArea = function(radius){
+//     return 3.14*radius**2;
+// };
+// const area = calcArea(10);
+// console.log('Answer is: ', area);
+
+// ARROW FUNCTION
+// const calcArea = (radius) => 3.14*radius**2;
+
+// const area = calcArea(10);
+// console.log('Answer is: ', area);
+
+// ARROW FUNCTION EXERCISE
+// const bill = function(products, tax){
+//     let total=0;
+//     for (let i = 0; i < products.length; i++){
+//         total += products[i] + products[i]*tax;
+//     }
+//     return total;
+// }
+// console.log(bill([10,15,30], 0.2));
+
+// << THIS WILL GIVE:
+// const bill = (products, tax) => {
+//     let total=0;
+//     for (let i = 0; i < products.length; i++){
+//         total += products[i] + products[i]*tax;
+//     }
+//     return total;
+// }
+// console.log(bill([10,15,30], 0.2));
