42483ef43907fbd81f489c7eef499a675c5c274f
Safaa <safaaeltom@gmail.com>
2026-05-16 14:59:51 +0300

    Add revision notes for functions vs methods differences

---
+++ Functions & Methods/sandbox.js
@@ -0,0 +1,10 @@
+const name = 'shaun';
+
+// FUNCTIONS
+const greet = () => 'Hello';
+let resultOne = greet();
+console.log(resultOne);
+
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
