993d0cb626966ac81aae417b5ae445b84988786a
Safaa <safaaeltom@gmail.com>
2026-05-17 23:40:01 +0300

    Complete javascript forEach method revision

---
+++ Foreach Method/sandbox.js
@@ -0,0 +1,21 @@
+// let people = ['mario', 'luigi', 'ryu', 'shaun'];
+
+// people.forEach(person=>{
+//     console.log(person);
+// });
+
+// let people = ['mario', 'luigi', 'ryu', 'shaun'];
+
+// people.forEach((person, index)=>{
+//     console.log(index, person);
+// });
+
+// let people = ['mario', 'luigi', 'ryu', 'shaun'];
+
+// const logPerson = (person, index) => {
+//     console.log(`${index} - hello ${person}`);
+// };
+
+// people.forEach(logPerson);
+
+
