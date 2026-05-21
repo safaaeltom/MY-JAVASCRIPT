2d71f8980ecba51db984816ce9668d45963c92d5
Safaa <safaaeltom@gmail.com>
2026-05-21 17:43:30 +0300

    Finish learning javascript object methods basics

---
+++ Object Methods/sandbox.js
@@ -0,0 +1,21 @@
+// Object methods
+// let user = {  // This ia an object
+//     name: 'crystal', // This property is a key value pair
+//     age: 30,
+//     email: 'crystal@thenetninja.co.uk',
+//     location: 'berlin',
+//     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+//     login: function(){
+//         console.log('The user logged in');
+//     },
+//     logout: function(){
+//         console.log('The user logged out');
+//     }
+// };
+
+// user.login(); // This is a method which is a function
+// user.logout();
+
+// const name = 'mario';
+// name.toUpperCase(); // This is a method which is a function
+
