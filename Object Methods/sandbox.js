56acfd4007e6530a65b039569aa232390b6efd6b
Safaa <safaaeltom@gmail.com>
2026-05-22 12:19:50 +0300

    Connect object methods to real webpage application

--- Object Methods/sandbox.js
+++ Object Methods/sandbox.js
@@ -20,20 +20,43 @@
 // name.toUpperCase(); // This is a method which is a function
 
 // (THIS) a method to print out all blogs in the array
-let user = {  // This ia an object
-    name: 'crystal', // This is a property (a key value pair)
-    email: 'crystal@thenetninja.co.uk',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+// let user = {  // This ia an object
+//     name: 'crystal', // This is a property (a key value pair)
+//     email: 'crystal@thenetninja.co.uk',
+//     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
     
-    // replace regular code with shorthand syntax for methods
-    // logBlogs: function(){
-    logBlogs(){
-        console.log('This user has written the following blogs:');
-        this.blogs.forEach(blog =>{
-            console.log(blog);
-        })
+//     // replace regular code with shorthand syntax for methods
+//     // logBlogs: function(){
+//     logBlogs(){
+//         console.log('This user has written the following blogs:');
+//         this.blogs.forEach(blog =>{
+//             console.log(blog);
+//         })
+//     }
+// };
+
+// user.logBlogs();
+// console.log(this);
+
+const ul = document.querySelector('.blogs');
+
+let user = {
+    name: 'crystal',
+    email: 'crystal@thenetninja.co.uk',
+    blogs: [
+        'why mac & cheese rules',
+        '10 things to make with marmite'
+    ],
+
+    logBlogs() {
+
+        let html = '';
+
+        this.blogs.forEach(blog => {
+            html += `<li>${blog}</li>`;
+        });
+        ul.innerHTML = html;
     }
 };
 
 user.logBlogs();
-console.log(this);
