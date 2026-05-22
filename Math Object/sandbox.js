09c78eee49115d14b7a1e2f6fa153d6b424932d9
Safaa <safaaeltom@gmail.com>
2026-05-22 17:20:24 +0300

    Complete javascript math object and random number generation revision

---
+++ Math Object/sandbox.js
@@ -0,0 +1,17 @@
+// Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.6;
+
+console.log(Math.round(area)); // to the nearest whole integer
+console.log(Math.floor(area)); // rounds it down
+console.log(Math.ceil(area)); // rounds it up
+console.log(Math.trunc(area)); // takes away the decimal
+
+// generate random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random));
+console.log(Math.round(random*100));
