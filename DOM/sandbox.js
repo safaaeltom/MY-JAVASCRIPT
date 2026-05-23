25ff63edd60b6c220c906cac1898585d5d6d8c42
Safaa <safaaeltom@gmail.com>
2026-05-23 17:47:39 +0300

    Study querySelector and querySelectorAll DOM methods

---
+++ DOM/sandbox.js
@@ -0,0 +1,27 @@
+// const para = document.querySelector('p');
+// console.log(para);
+
+// const para = document.querySelector('.error');
+// console.log(para);
+
+// const para = document.querySelector('div.error');
+// console.log(para);
+
+// To get the CSS selector, select the text you want in your webpage and inspect it. 
+// On the console (elements) right-click it and click copy then choose copy selector. 
+// Paste the selector in you javascript file, you will get your CSS selector.
+
+// const paras = document.querySelectorAll('p');
+// console.log(paras);
+
+// const paras = document.querySelectorAll('p');
+// console.log(paras[0]);
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para=>{
+//     console.log(para);
+// });
+
+// const errors = document.querySelectorAll('.error');
+// console.log(errors);
+
