b61e91277afd68d79d09dcfa07f09bbd9c09bc97
Safaa <safaaeltom@gmail.com>
2026-05-24 22:36:43 +0300

    Explore non-destructive style update and removel using element.style

---
+++ Style attribute/sandbox.js
@@ -0,0 +1,13 @@
+//const title = document.querySelector('h1');
+// this over-writes whats already there
+//title.setAttribute('style', 'margin: 50px')
+//console.log(title.style);
+//console.log(title.style.color);
+
+// this doesn't over-write the style
+// title.style.margin='50px';
+// title.style.color='crimson';
+// title.style.fontSize='40px';
+
+// to delete a style
+//title.style.fontSize='';
