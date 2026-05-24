b4c4e6af960f03917630b707ccb38b65a072fd69
Safaa <safaaeltom@gmail.com>
2026-05-24 12:44:03 +0300

    Practice appending and replacing webpage content using innerHTML

--- DOM text/sandbox.js
+++ DOM text/sandbox.js
@@ -12,8 +12,18 @@
 // console.log(content.innerHTML);
 // content.innerHTML += '<h2> THIS IS A NEW H2</h2>'
 
-const content = document.querySelector('.content');
-const people = ['mario', 'luigi', 'yoshi'];
-people.forEach(person=>{
-    content.innerHTML += `<p>${person}</p>`;
-})
+// const content = document.querySelector('.content');
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person=>{
+//     content.innerHTML += `<p>${person}</p>`;
+// })
+
+// const content = document.querySelector('.content');
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person=>{
+//     // content.innerHTML += `<p> Hello ${person}</p>`;
+
+// The webpage will loop through the names in the background
+// but will only show the last element of the array  
+//     content.innerHTML = `<p>This is the content ${person}</p>`; 
+// });
