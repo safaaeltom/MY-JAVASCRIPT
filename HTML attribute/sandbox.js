7ea9862e3a1b40592d1f7d3332114a5b4bfffc80
Safaa <safaaeltom@gmail.com>
2026-05-24 13:50:43 +0300

    Practice DOM getAttribute, setAttribute, and inline style manipulation

---
+++ HTML attribute/sandbox.js
@@ -0,0 +1,21 @@
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+// link.innerText = 'The net ninja website';
+
+// const msg = document.querySelector('p');
+// console.log(msg.getAttribute('class'));
+
+// msg.setAttribute('class', 'success');
+// msg.setAttribute('style', 'color: red')
+
+// now this will over write the style
+//msg.setAttribute('style', 'font-weight: bold');
+
+// add styles seperated by ;
+//msg.setAttribute('style', 'color: red; font-weight: bold; font-size: 30px');
+
+// or use real-world way .style instead to update styles seperately
+// msg.style.color = 'green';
+// msg.style.fontWeight = 'bold';
+// msg.style.fontSize = '30px';
