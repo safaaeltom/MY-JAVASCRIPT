951c9f24556345506b9d7814127735625cdc1178
Safaa <safaaeltom@gmail.com>
2026-05-27 20:53:25 +0300

    Experiment with DOM nodes and child elements

--- Nodes/sandbox.js
+++ Nodes/sandbox.js
@@ -1,9 +1,21 @@
-const banner = document.querySelector('#page-banner');
+// const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is:',banner.nodeType);
-console.log('#page-banner node name is:',banner.nodeName);
-console.log('#page-banner has child nodes:',banner.hasChildNodes());
+// console.log('#page-banner node type is:',banner.nodeType);
+// console.log('#page-banner node name is:',banner.nodeName);
+// console.log('#page-banner has child nodes:',banner.hasChildNodes());
 
-// clone a node
-const clonedBanner = banner.cloneNode(true); // pass true to give nested content
-console.log(clonedBanner.innerHTML);
+// // clone a node
+// const clonedBanner = banner.cloneNode(true); // pass true to give nested content
+// console.log(clonedBanner.innerHTML); // can use .children as well
+
+// const clonedBanner = banner.cloneNode(false); // pass false to give parent only
+// console.log(clonedBanner.innerHTML); // can use .children as well
+
+// experimenting with nodes
+// const list = document.querySelector('#book-list');
+// console.log('#book-list node name is:', list.nodeName);
+
+// const clonedList = list.cloneNode(true);
+// console.log(clonedList.innerHTML);
+// const clonedList = list.cloneNode(false);
+// console.log(clonedList.innerHTML);
