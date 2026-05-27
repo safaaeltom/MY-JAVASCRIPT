65886705fc79ae8969399d160b52f71a5bc44124
Safaa <safaaeltom@gmail.com>
2026-05-27 23:07:50 +0300

    Practice navigating previous and next sibling elements in the DOM

--- DOM traversal/sandbox.js
+++ DOM traversal/sandbox.js
@@ -5,3 +5,13 @@
 
 // console.log(bookList.childNodes);
 // console.log(bookList.children);
+
+// DOM traversal from sibling to sibling
+// const bookList = document.querySelector('#book-list');
+
+// console.log('book-list next sibling is:',bookList.nextSibling);
+// console.log('book-list next element sibling is:',bookList.nextElementSibling);
+// console.log('book-list previous sibling is:',bookList.previousSibling);
+// console.log('book-list previous element sibling is:',bookList.previousElementSibling);
+
+// bookList.previousElementSibling.querySelector('p').innerHTML += `<br/>Too cool for everyone else!`;
