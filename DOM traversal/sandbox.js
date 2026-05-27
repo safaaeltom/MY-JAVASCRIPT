94d7b0c0ec5392c4dcafe6235dfef0864f3b0beb
Safaa <safaaeltom@gmail.com>
2026-05-27 22:25:14 +0300

    Practice parent and child traversal using childNodes and children

---
+++ DOM traversal/sandbox.js
@@ -0,0 +1,7 @@
+// const bookList = document.querySelector('#book-list');
+
+// console.log('the parent node is:',bookList.parentNode);
+// console.log('the parent element is:',bookList.parentElement);
+
+// console.log(bookList.childNodes);
+// console.log(bookList.children);
