2d58525abfa3d3708c1bc8613a1d26adb3a1aa48
Safaa <safaaeltom@gmail.com>
2026-06-07 20:29:07 +0300

    feat: implement tabbed content functionality

--- Tapped content/sandbox.js
+++ Tapped content/sandbox.js
@@ -63,3 +63,23 @@ searchBar.addEventListener('keyup', function(e){
         }  
     })
 })
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == "LI"){
+        const targetPanel = document.querySelector(
+            e.target.dataset.box
+        );
+
+        panels.forEach(function(panel){
+            if(panel === targetPanel){
+                panel.classList.add('active');
+            }else{
+                panel.classList.remove('active')
+            }
+        })
+    }
+})
