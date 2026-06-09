c7b9d2c51607013e0014fa76fa883c7623ae17dc
Safaa <safaaeltom@gmail.com>
2026-06-09 23:36:54 +0300

    Introduction to asynchronous JavaScript

--- Asynchronous JS/sandbox.js
+++ Asynchronous JS/sandbox.js
@@ -1,9 +1,22 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 
-setTimeout(() => {
-    console.log('calback function fired')
-}, 2000);
+// setTimeout(() => {
+//     console.log('calback function fired')
+// }, 2000);
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+//  HTTP request
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () =>{
+//console.log(request, request.readyState);
+if(request.readyState === 4){
+    console.log(request.responseText);
+    }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
