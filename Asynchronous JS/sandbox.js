5724b64a7c9173494cb0f12d93d2b8f9d199eef3
Safaa <safaaeltom@gmail.com>
2026-06-10 21:21:32 +0300

    Encapsulate XMLHttpRequest logic in reusable function

--- Asynchronous JS/sandbox.js
+++ Asynchronous JS/sandbox.js
@@ -30,7 +30,30 @@
 // // Send the request to the server
 // request.send();
 
+// // Handle XHR completion and check successful responses
+// const request = new XMLHttpRequest(); 
+// // Listen for changes in the request state
+// request.addEventListener('readystatechange', () =>{
 
+// // Run this code when request is complete
+// if(request.readyState === 4 && request.status === 200){ // Request finished and successful
+    
+//     // Log the response returned by the API
+//     console.log(request, request.responseText);
+//     }else if(request.readyState === 4){
+//         console.log('could not fetch the data')
+//     }
+// });
+
+// // Make a GET request to a specific API endpoint
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+// // Send the request to the server
+// request.send();
+
+//
+const getTodos = () =>{
+
+// Request logic wrapped
 const request = new XMLHttpRequest(); 
 // Listen for changes in the request state
 request.addEventListener('readystatechange', () =>{
@@ -49,3 +72,6 @@ if(request.readyState === 4 && request.status === 200){ // Request finished and 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 // Send the request to the server
 request.send();
+};
+
+getTodos();
