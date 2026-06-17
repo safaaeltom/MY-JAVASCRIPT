bc4387a89b1961963b52c3a6acd746c871fdc581
Safaa <safaaeltom@gmail.com>
2026-06-17 09:17:38 +0300

    Create promises folder and add asset files

---
+++ Promises/sandbox.js
@@ -0,0 +1,28 @@
+const getTodos = (resource, callback) =>{
+
+const request = new XMLHttpRequest(); 
+request.addEventListener('readystatechange', () =>{
+
+if(request.readyState === 4 && request.status === 200){ 
+    const data = JSON.parse(request.responseText)
+    callback(undefined, data);
+    }else if(request.readyState === 4){
+    callback('could not fetch data', undefined);
+    }
+});
+
+request.open('GET', resource);
+request.send();
+};
+
+getTodos('todos/luigi.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/mario.json', (err, data) => {
+        console.log(data);
+        getTodos('todos/shaun.json', (err, data) => {
+            console.log(data);
+        });
+    });
+});
+
+
