2faf88eda92a327ae03b310fc64dab6ef4e8f212
Safaa <safaaeltom@gmail.com>
2026-06-17 21:00:29 +0300

    Convert callback-based XHR to Promise-based handling

--- Promises/sandbox.js
+++ Promises/sandbox.js
@@ -25,17 +25,47 @@
 //     });
 // });
 
-// promise example
-const getSomething = () => {
-    return new Promise((resolve, reject) => {
-        // use previous code to fetch something without callback hell part
-        resolve('some data');
-        //reject('some error');
+// // promise example
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         // use previous code to fetch something without callback hell part
+//         resolve('some data');
+//         //reject('some error');
+//     });
+// };
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err)
+// });
+
+
+// Apply promises to real world example
+const getTodos = (resource) =>{
+
+return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest(); 
+
+    request.addEventListener('readystatechange', () =>{
+
+      if(request.readyState === 4 && request.status === 200){ 
+        const data = JSON.parse(request.responseText)
+        resolve(data);
+      } else if(request.readyState === 4){
+        reject('error getting resource');
+      };
     });
+
+    request.open('GET', resource);
+    request.send();
+    });
+
 };
 
-getSomething().then(data => {
-    console.log(data);
+
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved:', data);
 }).catch(err => {
-    console.log(err)
-});
+    console.log('promise rejected:', err);
+})
