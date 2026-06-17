72a785e0833c7132cbf701bf6262dc0145fb3ada
Safaa <safaaeltom@gmail.com>
2026-06-17 09:53:46 +0300

    Create and consume JavaScript Promises

--- Promises/sandbox.js
+++ Promises/sandbox.js
@@ -1,28 +1,41 @@
-const getTodos = (resource, callback) =>{
+// const getTodos = (resource, callback) =>{
 
-const request = new XMLHttpRequest(); 
-request.addEventListener('readystatechange', () =>{
+// const request = new XMLHttpRequest(); 
+// request.addEventListener('readystatechange', () =>{
 
-if(request.readyState === 4 && request.status === 200){ 
-    const data = JSON.parse(request.responseText)
-    callback(undefined, data);
-    }else if(request.readyState === 4){
-    callback('could not fetch data', undefined);
-    }
-});
+// if(request.readyState === 4 && request.status === 200){ 
+//     const data = JSON.parse(request.responseText)
+//     callback(undefined, data);
+//     }else if(request.readyState === 4){
+//     callback('could not fetch data', undefined);
+//     }
+// });
 
-request.open('GET', resource);
-request.send();
+// request.open('GET', resource);
+// request.send();
+// };
+
+// getTodos('todos/luigi.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/mario.json', (err, data) => {
+//         console.log(data);
+//         getTodos('todos/shaun.json', (err, data) => {
+//             console.log(data);
+//         });
+//     });
+// });
+
+// promise example
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        // use previous code to fetch something without callback hell part
+        resolve('some data');
+        //reject('some error');
+    });
 };
 
-getTodos('todos/luigi.json', (err, data) => {
+getSomething().then(data => {
     console.log(data);
-    getTodos('todos/mario.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/shaun.json', (err, data) => {
-            console.log(data);
-        });
-    });
+}).catch(err => {
+    console.log(err)
 });
-
-
