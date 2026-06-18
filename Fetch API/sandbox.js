3ea29871e4cafd20c6e9292039dae02e385d6d36
Safaa <safaaeltom@gmail.com>
2026-06-18 19:48:43 +0300

    Connect Fetch API responses to DOM updates in real webpages

--- Fetch API/sandbox.js
+++ Fetch API/sandbox.js
@@ -8,3 +8,27 @@ fetch('todos/luigi.json').then((response) => {
 }).catch((err) => {
     console.log('rejected', err);
 });
+
+
+
+// real world pages
+fetch('medicines.json') // fetch data
+    .then(response => response.json()) // convert response to json
+    .then(data => {
+        displayMedicines(data); // use data
+    });
+
+// real world application
+fetch('todos.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const ul = document.querySelector('.todos');
+
+        data.forEach(todo => {
+            ul.innerHTML += `<li>${todo.text}</li>`;
+        });
+
+    });
+
+    // <ul class="todos"></ul> in html
