82a680adb1fd9088417da433a16bee7864354aa0
Safaa <safaaeltom@gmail.com>
2026-06-18 20:49:06 +0300

    Use async and await for asynchronous data fetching

--- Fetch API/sandbox.js
+++ Fetch API/sandbox.js
@@ -11,24 +11,38 @@ fetch('todos/luigi.json').then((response) => {
 
 
 
-// real world pages
-fetch('medicines.json') // fetch data
-    .then(response => response.json()) // convert response to json
-    .then(data => {
-        displayMedicines(data); // use data
-    });
+// // real world pages
+// fetch('medicines.json') // fetch data
+//     .then(response => response.json()) // convert response to json
+//     .then(data => {
+//         displayMedicines(data); // use data
+//     });
 
-// real world application
-fetch('todos.json')
-    .then(response => response.json())
-    .then(data => {
+// // real world application <ul class="todos"></ul> in html
+// fetch('todos.json')
+//     .then(response => response.json())
+//     .then(data => {
 
-        const ul = document.querySelector('.todos');
+//         const ul = document.querySelector('.todos');
 
-        data.forEach(todo => {
-            ul.innerHTML += `<li>${todo.text}</li>`;
-        });
+//         data.forEach(todo => {
+//             ul.innerHTML += `<li>${todo.text}</li>`;
+//         });
 
-    });
+//     });
 
-    // <ul class="todos"></ul> in html
+// async await data fetching
+const getTodos = async () => {
+    const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    return data;
+};
+
+console.log(1);
+console.log(2);
+
+getTodos()
+  .then(data => console.log('resolved:', data));
+
+console.log(3);
+console.log(4);
